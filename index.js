0ec2782216053d7dcab83f04312dfe6f832e6b29
RV411 <ivan_rodriguez3000@hotmail.com>
2022-09-03 07:55:31 -0500

    new file:   css/style.css new file:   images/01.png new file:   images/02.png new file:   images/03.png new file:   images/04.png new file:   images/05.png new file:   images/06.png new file:   images/07.png new file:   index.html new file:   index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+var findword="hola"
+const arrayWords=["hola","Estudiante","Programador"]
+
+arrayWords.forEach(element => {
+    if (findword==element)
+        console.log("Esta en el array");
+
+});
+
+
+
+
+
+
+
+
+
